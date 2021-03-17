b2ebed27404afa5a31777b6dbffa415a41cbf61e
PiotrTrawinski <piotr.trawinski.pl@gmail.com>
2021-03-17 12:24:38 +0100

    Added helper methods to BigInt class

--- src/BigInt/BigInt.h
+++ src/BigInt/BigInt.h
@@ -5,6 +5,7 @@
 #include "Unsigned64.h"
 
 struct BigInt {
+    BigInt() {}
     BigInt(uint64_t n) {
         _montgomeryNumberData = MontgomeryReductionMod<BigIntFixedSize<1>>(n);
     }
@@ -35,6 +36,11 @@ struct BigInt {
     BigInt(const std::string& str) {
         initializeModValue(str);
     }
+    BigInt& operator=(const BigInt& n) {
+        _montgomeryNumberData = n._montgomeryNumberData;
+        isMontgomeryFormInitialized = n.isMontgomeryFormInitialized;
+        return *this;
+    }
 
 
 
@@ -61,7 +67,71 @@ struct BigInt {
         return std::is_same_v<std::remove_cv_t<std::remove_reference_t<T>>, MontgomeryReductionMod<U>>;
     }
 
+    template<typename T> bool IsType() const {
+        return std::holds_alternative<MontgomeryReductionMod<T>>(_montgomeryNumberData);
+    }
+
+    template<typename T> T& get() {
+        return std::get<MontgomeryReductionMod<T>>(_montgomeryNumberData).mod;
+    }
+    template<typename T> const T& get() const {
+        return std::get<MontgomeryReductionMod<T>>(_montgomeryNumberData).mod;
+    }
+
+    int sizeInBits() const {
+        return visitNoInit([](auto&& a) { return a.mod.sizeInBits(); });
+    }
+
+    int typeIndex() const {
+        return _montgomeryNumberData.index();
+    }
+
+    bool isOne() const {
+        return visitNoInit([](auto&& a) { return a.mod == decltype(a.mod){ 1 }; });
+    }
+    
+    void fitToSize() {
+        auto expectedIndex = expectedTypeIndex(sizeInBits());
+        auto index = typeIndex();
+        if (index == expectedIndex) {
+            return;
+        }
+        switch (expectedIndex) {
+        case 0: fitToSize<BigIntGmp>(); break;
+        case 1: fitToSize<BigIntFixedSize<1>>(); break;
+        case 2: fitToSize<BigIntFixedSize<2>>(); break;
+        case 3: fitToSize<BigIntFixedSize<3>>(); break;
+        case 4: fitToSize<BigIntFixedSize<4>>(); break;
+        case 5: fitToSize<BigIntFixedSize<5>>(); break;
+        case 6: fitToSize<BigIntFixedSize<6>>(); break;
+        case 7: fitToSize<BigIntFixedSize<7>>(); break;
+        case 8: fitToSize<BigIntFixedSize<8>>(); break;
+        }
+    }
+
+    BigInt& operator/=(const BigInt& n) {
+        visitNoInit([&n](auto&& a) { n.visitNoInit([&a](auto&& b) {
+            div(a.mod, a.mod, decltype(a.mod)(b.mod));
+            //a /= b;
+        });});
+        isMontgomeryFormInitialized = false;
+        fitToSize();
+        return *this;
+    }
+
 private:
+    int expectedTypeIndex(int sizeInBits) {
+        if (sizeInBits < 64)  return 1;
+        if (sizeInBits < 128) return 2;
+        if (sizeInBits < 192) return 3;
+        if (sizeInBits < 256) return 4;
+        if (sizeInBits < 320) return 5;
+        if (sizeInBits < 384) return 6;
+        if (sizeInBits < 448) return 7;
+        if (sizeInBits < 512) return 8;
+        else return 0;
+    }
+
     void initializeIfNeeded() const {
         if (!isMontgomeryFormInitialized) {
             visitNoInit([&](auto&& a) {
@@ -87,6 +157,12 @@ private:
             std::get<MontgomeryReductionMod<BigIntGmp>>(_montgomeryNumberData).mod = BigIntGmp{ str };
         }
     }
+    template<typename T> void fitToSize() {
+        visitNoInit([&](auto&& n) {
+            _montgomeryNumberData = MontgomeryReductionMod<T>(T(n.mod));
+        });
+        isMontgomeryFormInitialized = false;
+    }
 
     mutable std::variant<
         MontgomeryReductionMod<BigIntGmp>,
@@ -101,6 +177,26 @@ private:
     > _montgomeryNumberData;
     bool isMontgomeryFormInitialized = false;
 };
+
+bool operator==(const BigInt& a, const BigInt& b) {
+    if (a.typeIndex() != b.typeIndex())
+        return false;
+    switch (a.typeIndex()) {
+    case 0: return a.get<BigIntGmp>() == b.get<BigIntGmp>();
+    case 1: return a.get<BigIntFixedSize<1>>() == b.get<BigIntFixedSize<1>>();
+    case 2: return a.get<BigIntFixedSize<2>>() == b.get<BigIntFixedSize<2>>();
+    case 3: return a.get<BigIntFixedSize<3>>() == b.get<BigIntFixedSize<3>>();
+    case 4: return a.get<BigIntFixedSize<4>>() == b.get<BigIntFixedSize<4>>();
+    case 5: return a.get<BigIntFixedSize<5>>() == b.get<BigIntFixedSize<5>>();
+    case 6: return a.get<BigIntFixedSize<6>>() == b.get<BigIntFixedSize<6>>();
+    case 7: return a.get<BigIntFixedSize<7>>() == b.get<BigIntFixedSize<7>>();
+    case 8: return a.get<BigIntFixedSize<8>>() == b.get<BigIntFixedSize<8>>();
+    }
+    return false;
+}
+bool operator!=(const BigInt& a, const BigInt& b) {
+    return !(a == b);
+}
 
 std::ostream& operator<<(std::ostream& out, const BigInt& a) {
     return out << a.toString();
